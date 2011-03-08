ffeee681a6fe8b0bd391f207497f395ab9ffef85
Kenneth Reitz <me@kennethreitz.com>
2011-03-08 01:10:17 -0500

    basic view setup

---
+++ musicsaver/views.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+import json
+from cPickle import dumps
+
+import requests
+
+from flask import (
+	request, session, redirect, url_for,
+	abort, render_template, flash, Flask
+)
+
+from musicsaver import app
+from musicsaver.models import AccessLog, URL
+
+
+
+
+def store(object):
+    """Stores Model instance in database permanently."""
+
+    db.session.add(object)
+    db.session.commit()
+
+
+@app.route('/get', methods=['GET'])
+def get_song():
+    """Receives song."""
+
+    url = request.args.get('p')
+    r = requests.get(url)
+    
+    return r.content
