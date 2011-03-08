4d067df2db1e6a4c36429d86b207cf0ee61e8afd
Kenneth Reitz <me@kennethreitz.com>
2011-03-08 01:10:09 -0500

    default configurations

---
+++ musicsaver/defaults.py
@@ -0,0 +1,25 @@
+"""
+Application Configuration 
+"""
+
+
+# Database
+
+# SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
+
+
+# Music Constraints
+
+SONGS_PER_MINUTE = 3
+SONGS_PER_HOUR = 16
+SONGS_PER_DAY = 180
+URL_EXPIRES_MINUTES = 15
+
+
+# Migration Fetching Information
+
+CREDS = {
+    'jamsspace.com': ('user', 'auth'),
+}
+
+
